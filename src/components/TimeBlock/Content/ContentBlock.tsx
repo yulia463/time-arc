51e43d0c67d566665860bb9c9c8013c2c0332bcb
yuliaslatvitskaya <yuli.slat@yandex.ru>
2025-08-15 14:44:25 +0200

    feat: add Content

--- src/components/TimeBlock/Content/ContentBlock.tsx
+++ src/components/TimeBlock/Content/ContentBlock.tsx
@@ -6,6 +6,7 @@ import gsap from 'gsap';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+
 function ContentBlock() {
     const timelineRef = useRef<HTMLDivElement | null>(null);
 
@@ -14,32 +15,40 @@ function ContentBlock() {
     }, []);
 
     return (
-        <div className={style.ContentContainer} ref={timelineRef}>
-            <Swiper
-                modules={[Navigation]}
-                navigation
-                spaceBetween={50}
-                slidesPerView={2}
-                breakpoints={{
-                    320: { slidesPerView: 1, spaceBetween: 20 },
-                    768: { slidesPerView: 2, spaceBetween: 30 },
-                    1024: { slidesPerView: 2, spaceBetween: 50 },
-                }}
-            >
-                <SwiperSlide>
-                    <div className={style.YearSlide}>
-                        <span className={style.YearBlue}>2015</span>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className={style.YearSlide}>
-                        <span className={style.YearPink}>2022</span>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
+        <div className={style.ContentContainer}>
+            <div className={style.DatesWrapper}>
+                <div className={style.FirstDate}>2022</div>
+                <div className={style.SecondDate}>2022</div>
+            </div>
+            <div className={style.GrayLine}></div>
+
+
+            {/*<div className={style.ContentContainer} ref={timelineRef}>*/}
+            {/*<Swiper*/}
+            {/*    modules={[Navigation]}*/}
+            {/*    navigation*/}
+            {/*    spaceBetween={50}*/}
+            {/*    slidesPerView={2}*/}
+            {/*    breakpoints={{*/}
+            {/*        320: { slidesPerView: 1, spaceBetween: 20 },*/}
+            {/*        768: { slidesPerView: 2, spaceBetween: 30 },*/}
+            {/*        1024: { slidesPerView: 2, spaceBetween: 50 },*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className={style.YearSlide}>*/}
+            {/*            <span className={style.YearBlue}>2015</span>*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*    <SwiperSlide>*/}
+            {/*        <div className={style.YearSlide}>*/}
+            {/*            <span className={style.YearPink}>2022</span>*/}
+            {/*        </div>*/}
+            {/*    </SwiperSlide>*/}
+            {/*</Swiper>*/}
         </div>
 
-);
+    );
 }
 
 export default ContentBlock;
