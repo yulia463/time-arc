6fb75ad47af9c4fa6d2f4b08825e57128ee6c58a
yuliaslatvitskaya <yuli.slat@yandex.ru>
2025-08-18 15:04:28 +0200

    feat: fix for git

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import TimelineBlock from '@/components/TimelineBlock';
-import { demoTimeline } from '@/data/timelineData';
+import {demoTimeline} from '@/data/timelineData';
 import '@/styles/global.scss';
 
 const App = () => {
-  return (
-    <div className="demo-grid">
-      <TimelineBlock data={demoTimeline} title="Исторические даты" initialIndex={5} instanceId="tb1" />
-      {/*<TimelineBlock data={demoTimeline} title="Исторические даты (второй блок)" initialIndex={1} instanceId="tb2" />*/}
-    </div>
-  );
+    return (
+        <div className="demo-grid">
+            <TimelineBlock data={demoTimeline} title="Исторические даты" initialIndex={5} instanceId="tb1"/>
+            {/*<TimelineBlock data={demoTimeline} title="Исторические даты (второй блок)" initialIndex={1} instanceId="tb2" />*/}
+        </div>
+    );
 };
 
 const container = document.getElementById('root')!;
-createRoot(container).render(<App />);
+createRoot(container).render(<App/>);
